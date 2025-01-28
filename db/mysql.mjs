091e74406c65e746f77eae352bdf2ad56a0749d7
Daniel Rodrigues Ribeiro <daniel.rodrigues@g.globo>
2025-01-28 16:50:32 -0300

    dialect corrigido e removido o script de dev

--- db/mysql.mjs
+++ db/mysql.mjs
@@ -1,19 +1,22 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-const connection = new Sequelize(process.env.SCHEMA, process.env.USER, process.env.PASSWORD, {
+const connection = new Sequelize(
+  process.env.SCHEMA,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
     host: process.env.HOST,
-    dialect: process.env.DATABASE
-})
+    dialect: "mysql",
+  }
+);
 
-try{
-    connection.authenticate()
-    console.log(`conectado ao ${process.env.DATABASE}!`)
+try {
+  connection.authenticate();
+  console.log(`autenticado com sucesso!`);
 } catch (error) {
-    console.error('Não foi possivel conectar ao banco', error)
+  console.error("Não foi possivel conectar ao banco", error);
 }
 
-
-
-export default connection
+export default connection;
